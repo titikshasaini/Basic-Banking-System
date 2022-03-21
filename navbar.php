7c50b01007774ef0e1d03616f3c5f99511ce6c15
titikshasaini <60317101+titikshasaini@users.noreply.github.com>
2022-03-21 17:29:41 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-info">
+      <a class="navbar-brand" href="index.php">THE SPARKS FOUNDATION BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php">New User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transaction</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction Details</a>
+              </li>
+          </div>
+       </nav>
